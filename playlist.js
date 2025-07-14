7bf3c239c7526dac8ab672344d665ad6af9251bd
Automatomation <82321998+Automatomation@users.noreply.github.com>
2025-07-14 20:17:37 +0200

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -3,54 +3,81 @@ const playlist = [
   {
     title: "Why Does Love Got to Be So Sad",
     artist: "Derek and the Dominoes",
+    album: "Layla and Other Assorted Love Songs",
+    year: 1970,
+    moods: ["bluesy", "upbeat"],
     url: "https://www.dropbox.com/scl/fi/ihwpj0ifvsop5y65xcfpa/Why-Does-Love-Got-to-Be-So-Sad.mp3?rlkey=i4slg3khi89jlh7wyboie41cm&st=fciyz1in&raw=1",
     cover: "assets/laylaandotherassortedlovesongs.jpg"
   },
   {
     title: "Back in Black",
     artist: "AC/DC",
+    album: "Back in Black",
+    year: 1980,
+    moods: ["upbeat"],
     url: "https://www.dropbox.com/scl/fi/f0m1l6veotnfncushagz5/Back-in-Black.mp3?rlkey=2a27glrq6woeil5op8ehb6x97&st=0lo4k4v4&raw=1",
     cover: "assets/backinblack.jpg"
   },
   {
     title: "Call Me The Breeze",
     artist: "J.J. Cale",
+    album: "Naturally",
+    year: 1971,
+    moods: ["bluesy", "chill"],
     url: "https://www.dropbox.com/scl/fi/6d94cd2mm636ave4t55fl/Call-Me-The-Breeze.mp3?rlkey=zge86keniqohp4uxq98yj1dx0&st=w2trhnfv&raw=1",
     cover: "assets/naturally.jpg"
   },
   {
     title: "Changes",
     artist: "Black Sabbath",
+    album: "Vol. 4",
+    year: 1972,
+    moods: ["melancholic", "heavy"],
     url: "https://www.dropbox.com/scl/fi/iywjgna52v9y9uaykr9ef/Changes.mp3?rlkey=eblpuisagn06ex00gyuq31z2u&st=5wp40xue&raw=1",
     cover: "assets/vol4.jpg"
   },
   {
     title: "Hells Bells",
     artist: "AC/DC",
+    album: "Back in Black",
+    year: 1980,
+    moods: ["upbeat"],
     url: "https://www.dropbox.com/scl/fi/cdup24w6b7flumq304ja2/Hells-Bells.mp3?rlkey=ptz8m5zb564gses448k9gi6be&st=umsv6qb1&raw=1",
     cover: "assets/backinblack.jpg"
   },
   {
     title: "Layla",
     artist: "Derek and the Dominoes",
+    album: "Layla and Other Assorted Love Songs",
+    year: 1970,
+    moods: ["bluesy", "upbeat"],
     url: "https://www.dropbox.com/scl/fi/ateyd1calr7yxa18bc69j/Layla.mp3?rlkey=u2uaheo0cbktbtsgh79pt7g4v&st=va78rum1&raw=1",
     cover: "assets/laylaandotherassortedlovesongs.jpg"
   },
   {
     title: "Mr. Crowley",
     artist: "Ozzy Osbourne",
+    album: "Blizzard of Ozz",
+    year: 1980,
+    moods: ["melancholic", "heavy"],
     url: "https://www.dropbox.com/scl/fi/48fazw6ot1nix3pdn13sl/Mr.-Crowley.mp3?rlkey=l0lrpibthj0c98px052pf7ekd&st=4v2u8hz1&raw=1",
     cover: "assets/blizzardofozz.jpg"
   },
   {
     title: "Paranoid",
     artist: "Black Sabbath",
+    album: "Paranoid",
+    year: 1970,
+    moods: ["heavy"],
     url: "https://www.dropbox.com/scl/fi/x1tuxd1iyp93rpsvzftxm/Paranoid.mp3?rlkey=i0jx4sujfq31buj1xozmr6noc&st=s9et4x67&raw=1",
     cover: "assets/paranoid.jpg"
   },
   {
     title: "War Pigs",
     artist: "Black Sabbath",
+    album: "Paranoid",
+    year: 1970,
+    moods: ["heavy"],
     url: "https://www.dropbox.com/scl/fi/i6ck516feez841tietit0/War-Pigs.mp3?rlkey=24n1cg6i9pejx1gd5ca55jzf1&st=kj09w36c&raw=1",
     cover: "assets/paranoid.jpg"
   }
